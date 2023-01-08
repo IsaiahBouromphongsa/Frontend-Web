abdaae2d4552c952ea8e135a3052b5056a9058d9
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-01-08 05:34:22 -0600

    Make the info be sent to database from input boxes

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddUser () {
-  return <div className='container'>
+  
+  const [user, setUsers]=useState({
+        name:"",
+        username:"",
+        email:""    
+  })
+
+  const {name, username, email}=user
+
+  const onInputChange=(e)=>{
+        setUsers({...user,[e.target.name]:e.target.value})
+  }
+
+  const onSubmit=(e)=>{
+        e.preventDefault(); //Stops the creation of weird looking URLs
+  }
+  
+  return (
+    <div className='container'>
             <div className="row">
                 <div className="row">
                     <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                         <h2 className="text-center m-4">Register User</h2>
+                        <form onSubmit={(e)=> onSubmit(e)}>
                         <div className="mb-3">
                             <label htmlFor="Name" className="form-label">
                                 Name
@@ -14,7 +33,10 @@ export default function AddUser () {
                                 type={"text"}
                                 className="form-control"
                                 placeholder="Enter your name"
-                                name ="name"/>
+                                name ="name"
+                                value={name}
+                                onChange={(e)=>onInputChange(e)} 
+                                />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="Username" className="form-label">
@@ -24,7 +46,10 @@ export default function AddUser () {
                                 type={"text"}
                                 className="form-control"
                                 placeholder="Enter your Username"
-                                name ="Username"/>
+                                name ="username"
+                                value={username}
+                                onChange={(e)=>onInputChange(e)} 
+                                />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="Email" className="form-label">
@@ -34,7 +59,10 @@ export default function AddUser () {
                                 type={"text"}
                                 className="form-control"
                                 placeholder="Enter your Email"
-                                name ="Email"/>
+                                name ="email"
+                                value={email}
+                                onChange={(e)=>onInputChange(e)} 
+                                />
                         </div>
                         <button type="Submit" className="btn btn-outline-primary">
                             Submit
@@ -42,9 +70,10 @@ export default function AddUser () {
                         <button type="Cancel" className="btn btn-outline-danger mx-2">
                             Cancel
                         </button>
+                        </form>
                     </div>
                 </div>
             </div>
          </div>
-  
+  );
 }
