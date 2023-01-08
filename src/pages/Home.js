6fb13038bdb712e10661eb3be18c5ff4d094ac6e
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-01-07 23:36:36 -0600

    Create a navbar using bootstrap

---
+++ src/pages/Home.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { async } from 'q';
+
+export default function Home() {
+
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        loadUsers();
+    },[])
+
+    const loadUsers=async()=>{
+        const result=await axios.get("http://localhost:8080/users");
+        console.log(result.data);
+    }
+
+  return (
+    <div className='container'>
+        <div className = 'py-4'>
+        <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+        users.map((user, index)=>(
+            <tr>
+                <th scope="row" key={index}>{indx+1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+        </tr>
+        ))
+    }
+
+    
+  </tbody>
+</table>
+        </div>
+    </div>
+  )
+}
