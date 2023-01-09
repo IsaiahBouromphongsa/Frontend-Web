729459e05df77f145d0147e0ae799aa10d75bde4
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-01-09 01:13:19 -0600

    Define the input variables

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -32,12 +32,15 @@ export default function ViewUser() {
                         <ul className='list-group list-group-flush'>
                             <li className='list-group-item'>
                                 <b>Name:</b>
+                                {user.name}
                             </li>    
                             <li className='list-group-item'>
                                 <b>Username:</b>
+                                {user.username}
                             </li> 
                             <li className='list-group-item'>
                                 <b>Email:</b>
+                                {user.email}
                             </li> 
                         </ul>
                     </div>
